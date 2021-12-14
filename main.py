b95cc71e88906e8d2c379174519607bbc7e279cb
uncle-leeeeee <1126822354@qq.com>
2021-12-14 15:41:55 +0800

    update

--- main.py
+++ main.py
@@ -2,15 +2,11 @@ import pandas as pd
 import numpy as np
 import cx_Oracle
 import os
-os.environ['path'] = r'D:\software\instantclient_11_2'  # 设置instantclient环境变量位置
-os.environ['NLS_LANG'] = 'SIMPLIFIED CHINESE_CHINA.UTF8'
-print("cx_Oracle.version:", cx_Oracle.version)
+os.environ['path'] = r'D:\software\instantclient_12_1'  # 设置instantclient环境变量位置
 connection = cx_Oracle.connect(
-    'hgxt', 'hgxt_gx#11', cx_Oracle.makedsn('10.0.109.9', '1521', 'sjzx9'))
+    'hgxt', 'hgxt_gx#11', '10.0.109.9:1521/ORCL')  # 需要以SYSDBA身份进行登录，否则会报错
 
-cursor = cx_Oracle.Cursor(connection)
-print('连接成功')
-s1 = 'select * from qzj_tar.V_D_O_DLYS_KEHUOYUNYEHU'
-yzk = pd.read_sql(s1, connection)
-yzk.to_csv('yzk.csv')
-print('输出成功')
+# 遍历六月份的gps数据，根据起始点经纬度，计算每个物流园区的到发量（由距离公式，如果到圆心距离小于3km，则属于从该点出发或到达该点）
+s1 = 'select startpx,startpy,endpx,endpy from GPS_OUTPUT_RAW_202106_T1 group by startpx,startpy,endpx,endpy'
+car_SE = pd.read_sql(s1, connection)
+car_SE = car_SE[['STARTPX', 'STARTPY', 'ENDPX', 'ENDPY']].values
